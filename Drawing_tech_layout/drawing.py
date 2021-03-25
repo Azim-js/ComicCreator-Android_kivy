bde7871ae539a0b702eb8d9edc2ffb435707832e
Azim-js <az1234im@gmail.com>
2021-03-25 22:45:47 +0530

    layput_tech_design for rectangle

---
+++ Drawing_tech_layout/drawing.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.lang import Builder
+
+Builder.load_file("design.kv")
+
+class DrawingSpace(RelativeLayout):
+    pass
+
+class DrawingApp(App):
+    def build(self):
+        return DrawingSapce()
+
+if __name__=="__main__":
+    DrawingApp().run()        
